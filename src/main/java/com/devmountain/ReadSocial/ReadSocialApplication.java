bf57a5e742f86f902ca2400b24a5d9a759006359
ckyle121 <carolinekyle21@gmail.com>
2022-11-22 10:49:00 -0600

    spring initalizr

---
+++ src/main/java/com/devmountain/ReadSocial/ReadSocialApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.ReadSocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmountain/ReadSocial/ReadSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmountain.ReadSocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
