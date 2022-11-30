f42d582192e77d482a36c8583224bed5e395e5cc
ckyle121 <carolinekyle21@gmail.com>
2022-11-30 16:21:01 -0600

    create book entitiy

--- src/main/java/com/devmountain/ReadSocial/entities/Book.java
+++ src/main/java/com/devmountain/ReadSocial/entities/Book.java
@@ -1,4 +1,24 @@
 package com.devmountain.ReadSocial.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Books")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "title")
+    private String title;
+
+    @Column(columnDefinition = "poster")
+    private Long poster; 
 }
