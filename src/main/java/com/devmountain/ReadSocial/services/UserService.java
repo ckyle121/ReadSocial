88c7432815d473e0de03fc71da4b788e9bbd02d3
ckyle121 <carolinekyle21@gmail.com>
2022-12-05 14:33:57 -0600

    create user service

---
+++ src/main/java/com/devmountain/ReadSocial/services/UserService.java
@@ -0,0 +1,13 @@
+package com.devmountain.ReadSocial.services;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+import com.devmountain.ReadSocial.dtos.UserDto;
+
+public interface UserService {
+    @Transactional
+    List<String> adduser(UserDto userDto);
+
+    List<String> userLogin(UserDto userDto);
+}
