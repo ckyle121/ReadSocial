7a3ac5b1ec92dad34777ff13248d5e046f9862a8
ckyle121 <carolinekyle21@gmail.com>
2022-12-11 16:35:46 -0600

    create HTML pages and Logout Functionality

---
+++ src/main/resources/static/javascript/logout.js
@@ -0,0 +1,6 @@
+function handleLogout(){
+    let c = document.cookie.split(";");
+    for(let i in c){
+        document.cookie = /^[^=]+/.exec(c[i])[0]+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
+    }
+}
