58934f7accc948c7f5cce7f1d5864551c01ac037
ckyle121 <carolinekyle21@gmail.com>
2022-12-13 17:07:46 -0600

    add navbar to HTML pages

--- src/main/resources/static/javascript/logout.js
+++ src/main/resources/static/javascript/logout.js
@@ -3,4 +3,5 @@ function handleLogout(){
     for(let i in c){
         document.cookie = /^[^=]+/.exec(c[i])[0]+"=;expires=Thu, 01 Jan 1970 00:00:00 GMT"
     }
+    document.location.replace("./home.html");
 }
